fdcfce54a063b778cdeb697ea9dd5daafee0e1ac
martina <marti.lanzi@gmail.com>
2019-05-02 08:57:41 +0200

    Added bspline class dataset

--- dataset.py
+++ dataset.py
@@ -136,6 +136,93 @@ class ModelNetDataset(data.Dataset):
     def __len__(self):
         return self.lengths.sum()
 
+class bsplineDataset(data.Dataset):
+    
+    def __init__(self,
+                 root,
+                 npoints=2500,
+                 split='train_files',
+                 data_augmentation=True):
+        self.npoints = npoints
+        self.root = root
+        self.split = split
+        self.data_augmentation = data_augmentation
+        self.fns = []
+        #print(self.root)
+        with open(os.path.join(root, '{}.txt'.format(self.split)), 'r') as f:
+            for line in f:
+                self.fns.append(line.strip())
+        #print(self.fns)
+
+        self.lengths = []
+        for fn in self.fns:
+            data = np.load(fn)
+            #print(fn)
+            #nome_file = os.path.join(root,fn)
+            #print(nome_file)
+            #f = h5py.File(nome_file)
+            #self.lengths += len(f['label'][:])
+            self.lengths.append(len(data))
+        self.lengths = np.array(self.lengths)
+        #print(self.lengths[:1].sum())
+
+
+        self.cat = {}
+        with open(os.path.join(root, 'bspline_id.txt'), 'r') as f:
+            for line in f:
+                ls = line.strip().split()
+                self.cat[ls[0]] = int(ls[1])
+
+
+
+        self.classes = list(self.cat.keys())
+        print(self.classes)
+
+
+
+
+        self.final_data = np.ndarray([])
+        self.final_label = np.ndarray([])
+        
+        for fn in self.fns:
+            data = np.load(fn)
+            split_1 = fn.split('/')
+            last = split_1[-1]
+            split_2 = last.split('-')
+            number_subject = split_2[-1].split('.')[0]
+            file_label = "/Users/martina/Desktop/uniTrento/deep_learning/progetto_cimec/data_tractogram_cleaning/derivatives/reduced_tract_labels/sub-%s/labels_%s_reduced.txt" % (number_subject,number_subject)
+            with open(file_label) as f:
+                content = f.readlines()
+            label = [x.strip() for x in content]
+            label = np.asmatrix(label).T
+
+            
+            if not self.final_data.shape:
+                self.final_data = data
+                self.final_label = label
+            else:
+                self.final_data = np.concatenate((self.final_data,data))
+                self.final_label = np.concatenate((self.final_label,label))
+
+            
+    
+    def __getitem__(self, idx):
+        
+        pts = self.final_data[idx]
+        cls = self.final_label[idx]    
+
+        pts = torch.from_numpy(pts.astype(np.float32))
+        cls = torch.from_numpy(np.array([cls]).astype(np.int64))
+
+        return pts,cls
+
+
+    def __len__(self):
+        return self.lengths.sum()
+
+
+
+
 if __name__ == '__main__':
     dataset = sys.argv[1]
     datapath = sys.argv[2]
@@ -163,4 +250,14 @@ if __name__ == '__main__':
         #print('test')
         print(len(d))
         print(d[0])
+
+    if dataset == 'bspline':
+        #print('test')
+        #gen_modelnet_id(datapath)
+        #print(datapath)
+
+        d = bsplineDataset(root=datapath)
+        #print('test')
+        print(len(d))
+        print(d[1472115])
 
