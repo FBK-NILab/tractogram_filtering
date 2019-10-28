92b1894d5464784999e8cd914ca1d0257cd17e03
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-10-28 12:57:53 +0100

    Updating importing models

--- train_dsl.py
+++ train_dsl.py
@@ -26,7 +26,7 @@ from cluster_losses import CenterLoss, CSimLoss, DistLoss, GFLoss, HLoss
 #from pointnet_mgf import (OnlyFC, PointNetClsMultiFeat, PointNetDenseClsLocal,
                           #PointNetDenseClsMultiFeat,
                           #PointNetDenseClsMultiFeatMultiLayer, mean_mod)
-from models import PNbatch, PointNetCls, ST_loss
+from models import PMemb, PNbatch, PointNetPyg, GCNConvNet, NNConvNet, ST_loss
 from tensorboardX import SummaryWriter
 from twounit_net import TwoUnitNet
 from utils import get_spaced_colors
