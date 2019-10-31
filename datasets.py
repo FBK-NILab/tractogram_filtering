f2f8c4de8ed138c19ce1994616de87b101d28968
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-10-31 17:13:40 +0100

    removed print statement hcp20

--- datasets.py
+++ datasets.py
@@ -121,7 +121,7 @@ class HCP20Dataset(gDataset):
         sample['points'] = graph_sample
 
         #print('time building graph %f' % (time.time()-t0))
-        print(sample)
+        #print(sample)
         return sample
     
 class RndSampling(object):
