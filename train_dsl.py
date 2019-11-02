b0c56b0b9e5f11ba6b8f5622983cf76467093c72
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-02 17:15:31 +0100

    fixed bvec

--- train_dsl.py
+++ train_dsl.py
@@ -98,7 +98,7 @@ def train_iter(cfg, dataloader, classifier, optimizer, writer, epoch, n_iter, cl
             data_list = []
             name_list = []
             for i,d in enumerate(sample_batched):
-                if 'bvec' in points.keys():
+                if 'bvec' in d.keys():
                     d.bvec += sample_size * i
                 data_list.append(d['points'])
                 name_list.append(d['name'])
