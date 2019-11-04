06290c4d33255f6c1ad9f208d28d509b18d84db9
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-04 16:48:11 +0100

    changed pool_op pn_geom

--- train_dsl.py
+++ train_dsl.py
@@ -46,7 +46,7 @@ def get_model(cfg):
         classifier = PNptg(input_size,
                                 int(cfg['embedding_size']),
                                 num_classes,
-                                pool_op=torch.max,
+                                pool_op=global_max_pool,
                                 batch_size=int(cfg['batch_size']),
                                 same_size=cfg['same_size'])
     return classifier
