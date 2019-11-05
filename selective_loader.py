f1848d514a0109249393b1ab96fce76e6a2b7dcd
Pietro Astolfi <pietroastolfi92@gmail.com>
2019-11-05 14:17:40 +0100

    fixed selective loader for python 3

--- selective_loader.py
+++ selective_loader.py
@@ -1,3 +1,4 @@
+import sys
 import nibabel as nib
 import os
 import struct
@@ -20,15 +21,24 @@ def load_selected_streamlines(trk_fn, idxs):
     point_size = 3 + n_scalars
     point_bytes = 4 * point_size
     properties_bytes = n_properties * 4
-
+    
     lengths = np.empty(nb_streamlines, dtype=np.int)
-    with open(trk_fn, 'rb') as f:
-        f.seek(header_size)
-        for idx in range(nb_streamlines):
-            l = np.fromfile(f, np.int32, 1)[0]
-            lengths[idx] = l
-            jump = point_bytes * l + properties_bytes
-            f.seek(jump, 1)
+    if float(sys.version[:3]) > 3.2:
+        with open(trk_fn, 'rb') as f:
+            f.seek(header_size)
+            for idx in range(nb_streamlines):
+                l = int.from_bytes(f.read(4), byteorder='little')
+                lengths[idx] = l
+                jump = point_bytes * l + properties_bytes
+                f.seek(jump, 1)
+    else:
+        with open(trk_fn, 'rb') as f:
+            f.seek(header_size)
+            for idx in range(nb_streamlines):
+                l = np.fromfile(f, np.int32, 1)[0]
+                lengths[idx] = l
+                jump = point_bytes * l + properties_bytes
+                f.seek(jump, 1)
 
     # position in bytes where to find a given streamline in the TRK file:
     index_bytes = lengths * point_bytes + properties_bytes + length_bytes
@@ -76,12 +86,13 @@ def load_selected_streamlines_uniform_size(trk_fn, idxs):
     lengths = np.empty(nb_streamlines, dtype=np.int)
     with open(trk_fn, 'rb') as f:
         f.seek(header_size)
-        for idx in range(nb_streamlines):
-            l = np.fromfile(f, np.int32, 1)[0]
-            lengths[idx] = l
-            jump = point_bytes * l + properties_bytes
-            f.seek(jump, 1)
-
+        #for idx in range(nb_streamlines):
+        l = np.fromfile(f, np.int32, 1)[0]
+        lengths[idx] = l
+        #    jump = point_bytes * l + properties_bytes
+        #    f.seek(jump, 1)
+    lengths[1:] = lengths[0]
+    
     # position in bytes where to find a given streamline in the TRK file:
     index_bytes = lengths * point_bytes + properties_bytes + length_bytes
     index_bytes = np.concatenate([[length_bytes], index_bytes[:-1]]).cumsum() + header_size
