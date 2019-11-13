2c57b6c6ea988bf6fec3129326703a2a6d8e31c5
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-13 23:01:52 +0100

    updated datasets.py

--- datasets.py
+++ datasets.py
@@ -85,7 +85,7 @@ class HCP20Dataset(gDataset):
     def getitem(self, idx):
         sub = self.subjects[idx]
         sub_dir = os.path.join(self.root_dir, 'sub-%s' % sub)
-        T_file = os.path.join(sub_dir, 'sub-%s_var-HCP_full_tract.trk' % (sub))
+        T_file = os.path.join(sub_dir, 'sub-%s_var-HCP_tract.trk' % (sub))
         label_file = os.path.join(sub_dir, 'sub-%s_var-HCP_labels.pkl' % (sub))
         #T_file = os.path.join(sub_dir, 'All_%s.trk' % (tract_type))
         #label_file = os.path.join(sub_dir, 'All_%s_gt.pkl' % (tract_type))
