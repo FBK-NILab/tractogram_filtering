6b4dde8044ed0c451ea08109d99318656129b668
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-13 23:09:11 +0100

    small tweaks

--- train_dsl.py
+++ train_dsl.py
@@ -420,8 +420,7 @@ def train(cfg):
       dataset = ds.HCP20Dataset(cfg['sub_list_train'],
                                 cfg['dataset_dir'],
                                 act=cfg['act'],
-                                transform=transforms.Compose(trans_train),
-                                return_edges=True)    
+                                transform=transforms.Compose(trans_train))    
     elif cfg['dataset'] == 'left_ifof_ss_sl_graph':
         dataset = ds.LeftIFOFSupersetGraphDataset(cfg['sub_list_train'],
                                 cfg['dataset_dir'],
@@ -450,8 +449,7 @@ def train(cfg):
             val_dataset = ds.HCP20Dataset(cfg['sub_list_val'], 
                                           cfg['val_dataset_dir'],
                                           act=cfg['act'],
-                                          transform=transforms.Compose(trans_val),
-                                          return_edges=True)
+                                          transform=transforms.Compose(trans_val))
         elif cfg['dataset'] == 'tractseg_500k':
             val_dataset = ds.Tractseg500kDataset(
                                     cfg['sub_list_val'],
