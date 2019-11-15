7b01679959587477e651abb68873f6503bb6c275
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-15 15:37:16 +0100

    updated models.py

--- models.py
+++ models.py
@@ -209,7 +209,7 @@ class GCNConvNet(torch.nn.Module):
         x = self.fc(F.relu(x))
         return x
     
-class NNC(torch.nn.Module):
+class NNC1(torch.nn.Module):
     def __init__(self, input_size, embedding_size, n_classes, batch_size=1, pool_op=global_max_pool, same_size=False):
         super(NNC, self).__init__()
         nn1 = nn.Sequential(nn.Linear(1, 32), nn.ReLU(), nn.Linear(32, input_size*32))
@@ -242,15 +242,15 @@ class NNC(torch.nn.Module):
         x = self.fc(F.relu(x))
         return x  
     
-class NNC1(torch.nn.Module):
+class NNC(torch.nn.Module):
     def __init__(self, input_size, embedding_size, n_classes, batch_size=1, pool_op=global_max_pool, same_size=False):
-        super(NNC1, self).__init__()
+        super(NNC, self).__init__()
         nn1 = nn.Sequential(nn.Linear(1, 32), nn.ReLU(), nn.Linear(32, input_size*32))
-        self.conv1_0 = NNConv(input_size, 32, nn1)
+        self.conv1_0 = NNConv(input_size, 32, nn1, aggr='mean')
         nn3 = nn.Sequential(nn.Linear(1, 32), nn.ReLU(), nn.Linear(32,32*64))
-        self.conv2_0 = NNConv(32,64, nn3)
+        self.conv2_0 = NNConv(32,64, nn3, aggr='mean')
         nn4 = nn.Sequential(nn.Linear(1, 32), nn.ReLU(), nn.Linear(32,64*embedding_size))
-        self.conv3 = NNConv(64, embedding_size, nn4)
+        self.conv3 = NNConv(64, embedding_size, nn4, aggr='mean')
          
         self.fc = torch.nn.Linear(embedding_size, n_classes)
         self.pool = pool_op
