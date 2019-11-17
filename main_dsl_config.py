4d2c6bf07e4f3839e86fe606c73aa82edbb76148
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-17 20:11:49 +0100

    updated configs

--- main_dsl_config.py
+++ main_dsl_config.py
@@ -7,7 +7,7 @@ prototypes_file = data/prototypes.trk
 # prototypes.trk saved in voxel coordinates
 
 ########### model ###########
-model = gcn
+model = nnc
 batch_norm = y
 bn_decay = y
 bn_decay_init = 0.5
@@ -231,7 +231,7 @@ multi_category = n
 ########### data ###########
 dataset = hcp20_graph
 dataset_dir = /home/pa/data/ExTractor_PRIVATE/derivatives/merge_shuffle_trk
-fixed_size = 6000
+fixed_size = 8000
 val_dataset_dir = /home/pa/data/ExTractor_PRIVATE/derivatives/merge_shuffle_trk
 sub_list_train = data/sub_list_HCP_train.txt
 sub_list_val = data/sub_list_HCP_val.txt
@@ -251,7 +251,7 @@ multi_category = n
 ignore_class = 0
 same_size = n
 
-experiment_name = gcn-loss_nll-data_hcp20_full_nogradacc_knngraph
+experiment_name = nnc-loss_nll-data_hcp20_full_nogradacc_cartesian
 
 ####### ************************************************************************
 ####### ************************************************************************
