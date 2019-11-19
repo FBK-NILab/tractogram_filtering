6bc54ac0a8d04dab161bcf9dae9157af726265d2
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-19 14:25:54 +0100

    updated test_dsl.py

--- test_dsl.py
+++ test_dsl.py
@@ -316,7 +316,7 @@ def test(cfg):
                 #print('points shape:',points['streamlines'].shape)
                 #print('streamlines:',
                 #data_dir = cfg['dataset_dir']
-                streamlines, head, leng, idxs = load_streamlines(data['dir']+data['name']+'.trk')
+                streamlines, head, leng, idxs = load_streamlines(data['dir']+'/'+data['name']+'.trk')
                 print('tract:',len(streamlines))
                 print('pred:',obj_pred_choice)
                 print('taget:',obj_target)
