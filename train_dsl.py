197e9a27cfe3444503b4c05422f169f44c8cf8a5
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-28 15:41:35 +0100

    typo

--- train_dsl.py
+++ train_dsl.py
@@ -522,7 +522,7 @@ def train(cfg):
                                 cfg['dataset_dir'],
                                 act=cfg['act'],
                                 transform=transforms.Compose(trans_train),
-                                self_loops=transforms.AddSelfLoops(),
+                                self_loops=T.AddSelfLoops(),
                                 #distance=T.Distance(norm=True,cat=False),
                                 return_edges=True)    
     elif cfg['dataset'] == 'left_ifof_ss_sl_graph':
@@ -555,7 +555,7 @@ def train(cfg):
                                           act=cfg['act'],
                                           transform=transforms.Compose(trans_val),
                                           #distance=T.Distance(norm=True,cat=False),
-                                          self_loops=transforms.AddSelfLoops(),
+                                          self_loops=T.AddSelfLoops(),
                                           return_edges=True)
         elif cfg['dataset'] == 'tractseg_500k':
             val_dataset = ds.Tractseg500kDataset(
