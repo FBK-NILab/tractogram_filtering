c237720450fc20d15e3ab338eda7492a350fb534
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-28 12:54:09 +0100

    updated datasets.py

--- datasets.py
+++ datasets.py
@@ -89,9 +89,9 @@ class HCP20Dataset(gDataset):
     def getitem(self, idx):
         sub = self.subjects[idx]
         sub_dir = os.path.join(self.root_dir, 'sub-%s' % sub)
-        #trk_dir = os.path.join(self.root_dir,'derivatives/streamlines_resampling_32p', 'sub-%s' % sub)
-        T_file = os.path.join(sub_dir, 'sub-%s_var-GIN-resampled_full_tract.trk' % (sub))
-        label_file = os.path.join(sub_dir, 'sub-%s_var-GIN_labels.pkl' % (sub))
+        trk_dir = os.path.join(self.root_dir,'derivatives/streamlines_resampled_16', 'sub-%s' % sub)
+        T_file = os.path.join(trk_dir, 'sub-%s_var-HCP-resampled_full_tract.trk' % (sub))
+        label_file = os.path.join(sub_dir, 'sub-%s_var-HCP_labels.pkl' % (sub))
         #T_file = os.path.join(sub_dir, 'All_%s.trk' % (tract_type))
         #label_file = os.path.join(sub_dir, 'All_%s_gt.pkl' % (tract_type))
         T = nib.streamlines.load(T_file, lazy_load=True)
