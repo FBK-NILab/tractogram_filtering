1863c6cdb890d5b31c70cc0dad2570621f7c6a1e
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-11-28 11:32:57 +0100

    typo

--- datasets.py
+++ datasets.py
@@ -91,7 +91,7 @@ class HCP20Dataset(gDataset):
         sub_dir = os.path.join(self.root_dir, 'sub-%s' % sub)
         #trk_dir = os.path.join(self.root_dir,'derivatives/streamlines_resampling_32p', 'sub-%s' % sub)
         T_file = os.path.join(sub_dir, 'sub-%s_var-GIN-resampled_full_tract.trk' % (sub))
-        label_file = os.path.join(sub_dir, 'sub-%s_var-HCP_labels.pkl' % (sub))
+        label_file = os.path.join(sub_dir, 'sub-%s_var-GIN_labels.pkl' % (sub))
         #T_file = os.path.join(sub_dir, 'All_%s.trk' % (tract_type))
         #label_file = os.path.join(sub_dir, 'All_%s_gt.pkl' % (tract_type))
         T = nib.streamlines.load(T_file, lazy_load=True)
