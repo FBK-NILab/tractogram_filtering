50c4332b756e30161e67ab70eef7c1fe05efde34
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-12-03 15:15:36 +0100

    updated configs

--- main_dsl_config.py
+++ main_dsl_config.py
@@ -251,7 +251,7 @@ multi_category = n
 ignore_class = 0
 same_size = y
 
-experiment_name = decseq2-loss_nll-data_hcp20_resampled16_full_nogradacc_fov4_k5
+experiment_name = decseq2-loss_nll-data_hcp20_resampled16_full_nogradacc_fov3_k5
 
 ####### ************************************************************************
 ####### ************************************************************************
