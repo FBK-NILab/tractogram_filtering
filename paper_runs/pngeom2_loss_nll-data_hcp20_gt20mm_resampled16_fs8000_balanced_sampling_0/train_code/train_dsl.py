56499c5e3e0abca90b14c75acd05fc00d844496d
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-12-04 11:42:37 +0100

    updated train_dsl.py

--- paper_runs/pngeom2_loss_nll-data_hcp20_gt20mm_resampled16_fs8000_balanced_sampling_0/train_code/train_dsl.py
+++ paper_runs/pngeom2_loss_nll-data_hcp20_gt20mm_resampled16_fs8000_balanced_sampling_0/train_code/train_dsl.py
@@ -119,7 +119,7 @@ def get_model(cfg):
                             fov=1,
                             dropout=0.5)    
     if cfg['model'] == 'dec':
-      classifier = DECSeq(input_size,
+      classifier = DECSeq5(input_size,
                        int(cfg['embedding_size']),
                        num_classes,
                        #fov=3,
