7e2a67b5f7414ab65f179f70546d634112571b22
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-12-06 11:41:55 +0100

    updated train_dsl.py

--- train_dsl.py
+++ train_dsl.py
@@ -117,7 +117,7 @@ def get_model(cfg):
       classifier = DEC(input_size,
                        int(cfg['embedding_size']),
                        num_classes,
-                       $fov=3,
+                       #fov=3,
                        batch_size=int(cfg['batch_size']),
                        k=5,
                        aggr='max',
