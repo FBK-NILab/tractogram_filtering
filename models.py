3adcb635ff8f07065243eaa9bf1a6976461913fd
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-12-07 13:43:59 +0100

    updated models.py

--- models.py
+++ models.py
@@ -728,8 +728,8 @@ class GATConvNet(torch.nn.Module):
         x = F.relu(self.conv1(x, data.edge_index))
         x = F.dropout(x, p=0.6, training=self.training)
         x = self.conv2(x, data.edge_index)
-        out = global_max_pool(x, data.batch)
-        out = self.lin(F.relu(out))
+        #out = global_max_pool(x, data.batch)
+        out = self.lin(F.relu(x))
         return out
 
 
