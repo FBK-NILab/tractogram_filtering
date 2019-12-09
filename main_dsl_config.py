5951d4e610dad68961f34f945480c9f291324030
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-12-09 21:57:32 +0100

    updated configs

--- main_dsl_config.py
+++ main_dsl_config.py
@@ -7,7 +7,7 @@ prototypes_file = data/prototypes.trk
 # prototypes.trk saved in voxel coordinates
 
 ########### model ###########
-model = dec
+model = blstm
 batch_norm = y
 bn_decay = y
 bn_decay_init = 0.5
@@ -251,7 +251,7 @@ multi_category = n
 ignore_class = 0
 same_size = y
 
-experiment_name = decseq5_improved_edges_loss_nll-data_hcp20_resampled16_full_nogradacc_k4
+experiment_name = bilstm_loss_nll-data_hcp20_resampled16_full_nogradacc
 
 ####### ************************************************************************
 ####### ************************************************************************
