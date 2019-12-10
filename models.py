8dde81dc7babf60df7e3054f89138a55178402d0
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-12-10 11:21:04 +0100

    updated models.py

--- models.py
+++ models.py
@@ -490,37 +490,8 @@ class DECSeq5(torch.nn.Module):
 
     def forward(self, data):
         pos, batch, eidx = data.pos, data.batch, data.edge_index
-        print(eidx)
-        print(eidx[0,-1])
-        print(eidx[0,:int(eidx.shape[1]/2)+2])
-        e1 = torch.repeat_interleave(eidx[0,:int(eidx.shape[1]/2)],self.k)
-        e1=torch.cat((e1,torch.repeat_interleave(eidx[0,-1],self.k)))
-        e2 = torch.tensor([],dtype=torch.long)
-        for i in list(eidx[0,:int(eidx.shape[1]/2)]):
-            print(i,e2)
-            if i == 0:
-                e2 = torch.cat([e2,torch.arange(i+1,self.k+1)],dim=0)
-            if i==1:
-                e2 = torch.cat([e2,torch.cat([torch.tensor([i-1]),torch.arange(i+1,self.k+1)],dim=0)])
-            if i<self.k/2 and i>1:
-                e2 = torch.cat([e2,torch.cat([torch.arange(0,i),torch.arange(i+1,i+(self.k-i)+1)],dim=0)])
-            if i>=self.k/2 and i!=eidx[0,int(eidx.shape[1]/2)-1]:
-                if i+self.k/2 > eidx[0,-1]:
-                    e2 = torch.cat([e2,torch.cat([torch.arange(i-(self.k-(eidx[0,-1]-i)),i),torch.arange(i+1,eidx[0,-1]+1)],dim=0)])
-                else:
-                    e = torch.cat([torch.arange(i-self.k/2,i),torch.arange(i+1,i+self.k/2+1)])
-                    e = e.long()
-                    print(e)
-                    e2 = torch.cat([e2,e])
-                    #e2 = torch.cat([e2,torch.cat([torch.arange(i-self.k/2,i),torch.arange(i+1,i+self.k/2+1)],dim=0)])
-            if i==eidx[0,int(eidx.shape[1]/2)-1]:
-                e2 = torch.cat([e2,torch.cat([torch.arange(i-1,i-self.k,-1),torch.tensor([i+1])])])
-        e2 = torch.cat([e2,torch.arange(eidx[0,-1]-1,(eidx[0,-1]-1)-self.k, -1)],dim=0)
-        e1, e2 = e1.cuda(), e2.cuda()
-        edges = torch.stack((e1,e2),0)
-        print('edges:',edges)
-        x1 = self.conv1(pos, edges)
-        x2 = self.conv2(x1, edges)
+        x1 = self.conv1(pos, eidx)
+        x2 = self.conv2(x1, eidx)
         out = self.lin1(torch.cat([x1, x2], dim=1))
         out = global_max_pool(out, batch)
         out = self.mlp(out)
