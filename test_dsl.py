8e50ba0e342d284d20fce6bc6a15c38dbcf7fd94
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2019-12-13 14:16:21 +0100

    updated test_dsl.py

--- test_dsl.py
+++ test_dsl.py
@@ -242,8 +242,8 @@ def test(cfg):
             print('length:',points['lengths'])
             print('k:',classifier.k)
             new_k = points['lengths']*(classifier.k/points['lengths'])
-            print('new k:',new_k)
-            classifier.conv1.k = new_k.cuda()
+            print('new k:',new_k,'integer k:',int(new_k))
+            classifier.conv1.k = int(new_k)
             if cfg['multi_category']:
                 one_hot_label = Variable(data['category'])
                 classifier.category_vec = one_hot_label.cuda()
