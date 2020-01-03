5518fc9c068206467ce90d622084df7598e10831
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-01-03 12:09:28 +0100

    updated configs

--- main_dsl_config.py
+++ main_dsl_config.py
@@ -231,7 +231,7 @@ multi_category = n
 ########### data ###########
 dataset = hcp20_graph
 dataset_dir = /home/pa/data/ExTractor_PRIVATE/derivatives/merge_shuffle_trk
-fixed_size = 8000
+fixed_size = 7000
 val_dataset_dir = /home/pa/data/ExTractor_PRIVATE/derivatives/merge_shuffle_trk
 sub_list_train = data/sub_list_HCP_train.txt
 sub_list_val = data/sub_list_HCP_val.txt
@@ -252,7 +252,7 @@ ignore_class = 0
 same_size = y
 
 #experiment_name = decseq1_loss_nll-data_hcp20_resampled16_full_nogradacc_
-experiment_name = decseq1_loss-nll_data-hcp20_16pts_k3
+experiment_name = decseq1_loss-nll_data-hcp20_20pts_k5_fs7000
 
 ####### ************************************************************************
 ####### ************************************************************************
