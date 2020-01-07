f63095510f867e2eeeb619901d1e66eec2052600
Pietro Astolfi <pietro@nilab-pietro>
2020-01-07 21:21:35 +0100

    decseqcos

--- models.py
+++ models.py
@@ -555,9 +555,6 @@ class DECSeqCos(torch.nn.Module):
     def forward(self, data):
         pos, batch, eidx = data.pos, data.batch, data.edge_index
         x1 = self.conv1(pos, eidx)
-        print(x1.shape)
-        print(batch.shape)
-        assert x1.dim() == 2 and batch.dim() == 1
         x2 = self.conv2(x1, batch)
         out = self.lin1(torch.cat([x1, x2], dim=1))
         out = global_max_pool(out, batch)
