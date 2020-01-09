c442c4165d19a92fcbacc7ce71d4c5d07fa83fc3
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-01-09 14:21:01 +0100

    updated test_dsl.py

--- test_dsl.py
+++ test_dsl.py
@@ -319,8 +319,8 @@ def test(cfg):
                 #print('points:',points['streamlines'])
                 #print('points shape:',points['streamlines'].shape)
                 #print('streamlines:',
-                data_dir = cfg['dataset_dir']
-                streamlines, head, leng, idxs = load_streamlines(data['dir']+'/'+data['name']+'.trk')
+                #data_dir = cfg['dataset_dir']
+                #streamlines, head, leng, idxs = load_streamlines(data['dir']+'/'+data['name']+'.trk')
                 #print('tract:',len(streamlines))
                 #print('pred:',obj_pred_choice)
                 #print('taget:',obj_target)
@@ -329,9 +329,9 @@ def test(cfg):
                 print('val max class red ', obj_pred_choice.max().item())
                 print('val min class pred ', obj_pred_choice.min().item())
                 y_pred = obj_pred_choice.cpu().numpy()
-                #np.save(data['dir']+'/y_pred_lstm_GIN',y_pred)
+                np.save(data['dir']+'/y_pred_dec_16pnts',y_pred)
                 y_test = obj_target.cpu().numpy()
-                #np.save(data['dir']+'/y_test_lstm_GIN',y_test)
+                np.save(data['dir']+'/y_test_dec_16pnts',y_test)
                 #np.save(data['dir']+'/streamlines_lstm_GIN',streamlines)
                 correct = obj_pred_choice.eq(obj_target.data.int()).cpu().sum()
                 acc = correct.item()/float(obj_target.size(0))
