4f1a16493ba1d91c4192ee822360db9453efc342
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-01-15 16:19:14 +0100

    updated train_dsl_cleaned.py

--- train_dsl_cleaned.py
+++ train_dsl_cleaned.py
@@ -53,11 +53,11 @@ def get_model(cfg):
             num_classes,
             dropout=cfg['dropout'],
             #fov=3,
-            #k=int(cfg['k_dec']),
-            k=5,
+            k=int(cfg['k_dec']),
+            #k=5,
             aggr='max',
-            pool_op=global_max_pool)
-            #pool_op=cfg['pool_op'])
+            #pool_op=global_max_pool)
+            pool_op=cfg['pool_op'])
         #bn=True)
     if cfg['model'] == 'nnc':
         classifier = NNC(input_size,
