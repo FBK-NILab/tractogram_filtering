441e9c7384d7bed799d4cf389378b900247710d1
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-01-17 11:04:04 +0100

    updated train_dsl_cleaned.py

--- train_dsl_cleaned.py
+++ train_dsl_cleaned.py
@@ -49,7 +49,7 @@ def get_model(cfg):
             input_size,
             int(cfg['embedding_size']),
             num_classes,
-            dropout=cfg['dropout'],
+            #dropout=cfg['dropout'],
             #fov=3,
             k=int(cfg['k_dec']),
             #k=5,
