689511f5611551a3b0cecb661e0503dc1d7f5593
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-01-17 10:56:13 +0100

    updated train_dsl_cleaned.py

--- train_dsl_cleaned.py
+++ train_dsl_cleaned.py
@@ -45,7 +45,7 @@ def get_model(cfg):
                               fov=1,
                               dropout=0.5)
     if cfg['model'] == 'dec':
-        classifier = DECSeq(
+        classifier = DEC(
             input_size,
             int(cfg['embedding_size']),
             num_classes,
