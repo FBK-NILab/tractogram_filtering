ee2f5447878d4fdb3c1d5beb48785b88084eaee3
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-01-27 15:52:30 +0100

    updated models.py

--- models.py
+++ models.py
@@ -26,10 +26,16 @@ from torch_geometric.nn import (DynamicEdgeConv, EdgeConv, GATConv, GCNConv,
 from torch_geometric.utils import add_self_loops, normalized_cut
 
 def MLP(channels, batch_norm=True):
-    return Seq(*[
-        Seq(Lin(channels[i - 1], channels[i]), ReLU(), BN(channels[i]))
+    if batch_norm:
+      return Seq(*[
+          Seq(Lin(channels[i - 1], channels[i]), ReLU(), BN(channels[i]))
+          for i in range(1, len(channels))
+      ])
+    else:
+      return Seq(*[
+        Seq(Lin(channels[i - 1], channels[i]), ReLU())
         for i in range(1, len(channels))
-    ])
+      ])
 
 class PNptg2(torch.nn.Module):
 
@@ -162,15 +168,15 @@ class BiLSTM(torch.nn.Module):
 class DECSeq(torch.nn.Module):
     def __init__(self, input_size, embedding_size, n_classes, dropout=True, k=5, aggr='max',pool_op='max'):
         super(DECSeq, self).__init__()
-        self.conv1 = EdgeConv(MLP([2 * 3, 64, 64, 64]), aggr)
-        self.conv2 = DynamicEdgeConv(MLP([2 * 64, 128]), k, aggr)
+        self.conv1 = EdgeConv(MLP([2 * 3, 64, 64, 64], batch_norm=True), aggr)
+        self.conv2 = DynamicEdgeConv(MLP([2 * 64, 128], batch_norm=True), k, aggr)
         self.lin1 = MLP([128 + 64, 1024])
         if pool_op == 'max':
             self.pool = global_max_pool
 
         if dropout:
             self.mlp = Seq(
-                MLP([1024, 512]), Dropout(0.5), MLP([512, 256]), Dropout(0.5),
+                MLP([1024, 512], batch_norm=True), Dropout(0.5), MLP([512, 256]), Dropout(0.5),
                 Lin(256, n_classes))
         else:
             self.mlp = Seq(
