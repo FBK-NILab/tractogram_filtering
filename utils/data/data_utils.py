b489ae05366bee5a53df78f303c30fd73f5ea2ed
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-04-11 18:57:05 +0200

    typo

--- utils/data/data_utils.py
+++ utils/data/data_utils.py
@@ -13,7 +13,7 @@ def get_dataset(cfg, trans, split_obj=False, train=True, test=False):
         batch_size = 1
         shuffling = False
     if test:
-        print('test')updat
+        print('test')
         sub_list = cfg['sub_list_test']
         batch_size = 1
         shuffling = False
