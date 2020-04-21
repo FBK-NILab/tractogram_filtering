c064b9f84c0880b592624539569461508e1cf5c2
rsverhagen94 <54837051+rsverhagen94@users.noreply.github.com>
2020-04-21 10:54:28 +0200

    updated selective_loader.py

--- utils/data/selective_loader.py
+++ utils/data/selective_loader.py
@@ -7,7 +7,7 @@ from time import time
 from nibabel.affines import apply_affine
 from nibabel.streamlines.trk import get_affine_trackvis_to_rasmm
 
-def load_selected_streamlines(trk_fn, idxs=None, return_scalars=True):
+def load_selected_streamlines(trk_fn, idxs=None, return_scalars=False):
 
     lazy_trk = nib.streamlines.load(trk_fn, lazy_load=True)
     header = lazy_trk.header
@@ -75,7 +75,7 @@ def load_selected_streamlines(trk_fn, idxs=None, return_scalars=True):
 
     return streams, lengths[idxs]
 
-def load_selected_streamlines_uniform_size(trk_fn, idxs=None, return_scalars=True):
+def load_selected_streamlines_uniform_size(trk_fn, idxs=None, return_scalars=False):
 
     lazy_trk = nib.streamlines.load(trk_fn, lazy_load=True)
     header = lazy_trk.header
