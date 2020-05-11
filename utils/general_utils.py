f3cd71a04738785bed8f15d16dcb1f49b3c936e4
rsverhagen <r.s.verhagen@tilburguniversity.edu>
2020-05-11 19:49:44 +0200

    commit on master

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -72,8 +72,8 @@ def update_metrics(metrics, prediction, target):
     prediction = prediction.data.int().cpu()
     target = target.data.int().cpu()
 
-    abs_err = torch.sum(abs(target-prediction))
-    mserr = torch.sum((target-prediction)**2)
+    abs_err = torch.sum(abs(target-prediction)).mean()
+    mserr = torch.sum((target-prediction)**2).mean()
     #correct = prediction.eq(target).sum().item()
     #acc = correct / float(target.size(0))
 
